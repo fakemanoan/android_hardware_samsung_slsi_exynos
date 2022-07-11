8f60368cb7673833b70081a17d32d9b0e8b49b2b
TALU <taluemail272@gmail.com>
2022-07-11 21:29:48 +0100

    gralloc: always just return 0 from check_for_compression() with no checks on the 7580
    In check_for_compression(), the value of the property ddk.set.afbc
    was checked. We don't have it set, and the 7580 doesn't support AFBC
    (Arm Frame Buffer Compression) so it can't be set to 1. If that property
    is set to 0, check_for_compression() always returned 0. So, cut the
    checks and simply always do that if the target SoC is the Exynos7580.
    
    The reading of the value of ddk.set.afbc causes SELinux denials under 12,
    so by doing this, the inconvenience of addressing them (properly with a
    new proper property context) just for it is avoided.
    
    Change-Id: I49399e49113c3dd0e24456e8e501f685ef64d790

--- gralloc/format_chooser.cpp
+++ gralloc/format_chooser.cpp
@@ -26,8 +26,9 @@
 #define GENERAL_UI (GRALLOC_USAGE_HW_TEXTURE | GRALLOC_USAGE_HW_COMPOSER)
 
 /* It's for compression check format, width, usage*/
-int check_for_compression(int w, int h, int format, int usage)
+int check_for_compression(int w __unused, int h __unused, int format __unused, int usage __unused)
 {
+#ifndef TARGET_SOC_EXYNOS7580
 	char value[256];
 	int afbc_prop;
 
@@ -62,6 +63,9 @@ int check_for_compression(int w, int h, int format, int usage)
 			return 0;
 	}
 
+#else
+	return 0;
+#endif
 }
 
 uint64_t gralloc_select_format(int req_format, int usage, int is_compressible)
